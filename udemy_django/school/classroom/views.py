85290cfc1a858af1c5cee290fd8466ede53824c5
KimUJeon <kinfe_411@naver.com>
2023-07-28 15:16:42 +0900

    섹션 14 - 96강: Django Forms(FormView)

--- udemy_django/school/classroom/views.py
+++ udemy_django/school/classroom/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.urls import reverse, reverse_lazy
+from django.views.generic import TemplateView, FormView
 
-# Create your views here.
-def home_view(request):
-    return render(request, "classroom/home.html")
+from classroom.forms import ContactForm
+
 
 class HomeView(TemplateView):
     template_name = "classroom/home.html"
 
 class ThankYouView(TemplateView):
     template_name = "classroom/thank_you.html"
+
+class ContactFormView(FormView):
+    form_class = ContactForm
+    template_name = 'classroom/contact.html'
+
+    # success URL?
+    success_url = reverse_lazy("classroom:thank_you")
+    # what to do with form?
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        # ContactForm(request.POST)
+        return super().form_valid(form)
