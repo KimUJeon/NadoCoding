a234a08b6edfe02ff758daa4121a82c4fc52f513
KimUJeon <kinfe_411@naver.com>
2023-07-31 13:35:47 +0900

    섹션 14 - 101강: Django Forms(DeleteView)

--- udemy_django/school/classroom/urls.py
+++ udemy_django/school/classroom/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import (HomeView, ThankYouView, ContactFormView, TeacherCreateView, TeacherListView, TeacherDetailView, TeacherUpdateView)
+from .views import (HomeView, ThankYouView, ContactFormView, TeacherCreateView, TeacherListView, TeacherDetailView, TeacherUpdateView, TeacherDeleteView)
 
 app_name = "classroom"
 
@@ -10,5 +10,6 @@ urlpatterns = [
     path('create_teacher/', TeacherCreateView.as_view(), name="create_teacher"),
     path('list_teacher/', TeacherListView.as_view(), name="list_teacher"),
     path('teacher_detail/<int:pk>', TeacherDetailView.as_view(), name="teacher_detail"),
-    path('update_teacher/<int:pk>', TeacherUpdateView.as_view(), name="update_teacher")
+    path('update_teacher/<int:pk>', TeacherUpdateView.as_view(), name="update_teacher"),
+    path('delete_teacher/<int:pk>', TeacherDeleteView.as_view(), name="delete_teacher")
 ]
--- udemy_django/school/classroom/views.py
+++ udemy_django/school/classroom/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
-from django.views.generic import TemplateView, FormView, CreateView, ListView, DetailView, UpdateView
+from django.views.generic import TemplateView, FormView, CreateView, ListView, DetailView, UpdateView, DeleteView
 from classroom.models import Teacher
 from classroom.forms import ContactForm
 
@@ -31,6 +31,10 @@ class TeacherUpdateView(UpdateView):
     model = Teacher
     fields = "__all__"
     success_url = reverse_lazy("classroom:update_teacher")
+
+class TeacherDeleteView(DeleteView):
+    model = Teacher
+    success_url = reverse_lazy("classroom:list_teacher")
 
 class ContactFormView(FormView):
     form_class = ContactForm
